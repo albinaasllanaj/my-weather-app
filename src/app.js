dace2f6693d7498c2a1d2e145a0a0ea9b7a2f26b
abbyaslla <112036955+abbyaslla@users.noreply.github.com>
2022-08-24 20:30:42 -0700

    Transferred my code to new folder "my-weather-app"

---
+++ src/app.js
@@ -0,0 +1,58 @@
+let now = new Date();
+
+let date = now.getDate();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+let day = days[now.getDay()];
+let milliseconds = now.getMilliseconds();
+
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+let today = document.querySelector("#todays-date");
+today.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function displayWeather(response) {
+  console.log(response);
+  console.log(response.data.name);
+  document.querySelector("#city").innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "1deefe5716d079b43ba8a49a72d134e2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#text-input").value;
+  search(city);
+}
+
+let form = document.querySelector("form");
+form.addEventListener("submit", handleSubmit);
+
+search("New York");
